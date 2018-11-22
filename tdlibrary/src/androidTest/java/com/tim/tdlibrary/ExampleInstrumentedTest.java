16bb66c44d5fb3560e4bb6005e0d36b4b32b1995
yang.ding <yang.ding@99bill.com>
2018-11-22 16:13:53 +0800

    add code

--- tdlibrary/src/androidTest/java/com/tim/tdlibrary/ExampleInstrumentedTest.java
+++ tdlibrary/src/androidTest/java/com/tim/tdlibrary/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.tim.widgetlibraby;
+package com.tim.tdlibrary;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.tim.widgetlibraby.test", appContext.getPackageName());
+        assertEquals("com.tim.tdlibrary.test", appContext.getPackageName());
     }
 }
--- tdlibrary/src/test/java/com/tim/tdlibrary/ExampleUnitTest.java
+++ tdlibrary/src/test/java/com/tim/tdlibrary/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.tim.widgetlibraby;
+package com.tim.tdlibrary;
 
 import org.junit.Test;
 
